257e5c1d4fc958b9bdad51623decf29b10f3003c
kotercy <31883359+kotercy9527@users.noreply.github.com>
2025-01-04 16:50:50 +0800

    补充一个方法

---
+++ KKit/KPerson.h
@@ -0,0 +1,17 @@
+//
+//  KPerson.h
+//  KKit
+//
+//  Created by zhipeng jiang on 2025/1/4.
+//  Copyright © 2025 kotercy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface KPerson : NSObject
+-(void)saySomething;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KKit/KPerson.m
@@ -0,0 +1,15 @@
+//
+//  KPerson.m
+//  KKit
+//
+//  Created by zhipeng jiang on 2025/1/4.
+//  Copyright © 2025 kotercy. All rights reserved.
+//
+
+#import "KPerson.h"
+
+@implementation KPerson
+-(void)saySomething {
+    NSLog(@"%s",__func__);
+}
+@end
