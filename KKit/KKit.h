1a249b42479f7a35f65dc8d49a395d443413ace4
kotercy <31883359+kotercy9527@users.noreply.github.com>
2025-01-04 16:41:53 +0800

    Initial Commit

---
+++ KKit/KKit.h
@@ -0,0 +1,18 @@
+//
+//  KKit.h
+//  KKit
+//
+//  Created by zhipeng jiang on 2025/1/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KKit.
+FOUNDATION_EXPORT double KKitVersionNumber;
+
+//! Project version string for KKit.
+FOUNDATION_EXPORT const unsigned char KKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KKit/PublicHeader.h>
+
+
